8ef4753c631ea37095c9156b2a77d54da4d0a1b8
nima <nimaaaaa2007@gmail.com>
2026-06-10 09:08:31 +0330

    add bullShit

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args){
-        System.out.println("test number 1");
+        System.out.println("oh babydoll");
+        System.out.println("I cant move on babydoll");
     }
 }
